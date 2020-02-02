ebff4520d815d044ee4fee51f4f7f9ed8f4f0b94
Jean Brito <jeanfbrito@gmail.com>
2020-02-02 15:01:28 -0300

    add medical records actions and views

--- app/controllers/medical_records_controller.rb
+++ app/controllers/medical_records_controller.rb
@@ -1,12 +1,43 @@
 class MedicalRecordsController < ApplicationController
   before_action :set_pet
+  before_action :set_medical_record, only: [:show, :edit, :update, :destroy]
   def index
     @medical_records = @pet.medical_records.order(:created_at)
+  end
+
+  def show
+  end
+
+  def new
+    @medical_record = @pet.medical_records.new
+  end
+
+  def create
+    @medical_record = @pet.medical_records.create(medical_record_params)
+    redirect_to pet_medical_record_path(@pet, @medical_record)
+  end
+
+  def update
+    @medical_record.update!(medical_record_params)
+    redirect_to pet_medical_record_path(@pet, @medical_record)
+  end
+
+  def destroy
+    @medical_record.delete
+    redirect_to pet_medical_records_path(@pet)
   end
 
   private
 
   def set_pet
     @pet = Pet.find(params[:pet_id])
+  end
+
+  def set_medical_record
+    @medical_record = @pet.medical_records.find(params[:id])
+  end
+
+  def medical_record_params
+    params.require(:medical_record).permit(:symtoms, :treatment)
   end
 end
