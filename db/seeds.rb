91334d0744e5b89b632993c684f77c92afa2f638
Jean Brito <jeanfbrito@gmail.com>
2020-02-02 12:18:36 -0300

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+  Pet.create(
+    name: Faker::Creature::Dog.name,
+    breed: Faker::Creature::Dog.breed,
+    kind: :dog,
+    birthdate: Faker::Date.between(from: 15.years.ago, to: Date.today)
+  )
+end
+
+50.times do
+  Pet.create(
+    name: Faker::Creature::Cat.name,
+    breed: Faker::Creature::Cat.breed,
+    kind: :cat,
+    birthdate: Faker::Date.between(from: 15.years.ago, to: Date.today)
+  )
+end
+
+Pet.all.each do |pet|
+  rand(0..5).times do
+    MedicalRecord.create(
+      pet: pet,
+      symtoms: Faker::TvShows::SiliconValley.quote,
+      treatment: Faker::TvShows::SiliconValley.quote
+    )
+  end
+end
